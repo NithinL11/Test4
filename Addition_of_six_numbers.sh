aa8ed16c48d70f03ae8e3a6e609ee8d00fd50130
root <root@LAPTOP-33N12T6F.>
2025-01-23 10:06:21 -0500

    Calculation steps

---
+++ Addition_of_six_numbers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Value_1=$1
+Value_2=$2
+Value_3=$3
+Value_4=$4
+Value_5=$5
+Value_6=$6
+
+# calculate the addition of numbers
+echo $((Value_1 + Value_2 + Value_3 + Value_4 + Value_5 + Value_6))
